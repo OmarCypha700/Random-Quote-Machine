6aab954ab14d3b2e97f527bb2f61a774a7720e34
OmarCypha700 <98944012+OmarCypha700@users.noreply.github.com>
2022-07-17 15:10:31 -0700

    update

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ class App extends Component {
 
     return (
       <div>
-        <h4 id='header'>Random Quote Generator</h4>
         <div
           id='quote-box'
           className="quote-box">
